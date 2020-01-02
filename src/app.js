917eb1bdcbd9128129c7fa4e0bf10ff5ad6eee7d
Brayan Steven Rendon <i@braren.co>
2020-01-02 16:25:48 -0500

    [#3] NPM & Yarn init setup

---
+++ src/app.js
@@ -0,0 +1,19 @@
+/**
+ * Class for init app runner.
+ * @author braren <i@braren.co>
+ * @copyright Copyright (c) 2020 Brayan Steven Rendón
+ * @class
+ */
+
+class App {
+  constructor () {
+    console.log('Starting...')
+  }
+
+  run () {
+    console.log('Started')
+  }
+}
+
+const app = new App()
+app.run()
